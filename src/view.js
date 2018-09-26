aca365fc7105bf739a72bc984af822764f8d8f10
Denny <denny@nusantara-cloud.com>
2018-09-26 11:24:41 +0700

    Avoid Invoking Modal Using ID
    Since there can be multiple instance of NCImagePicker on the same page,
    there can be conflicts if we show/hide modal through its id.

--- src/view.js
+++ src/view.js
@@ -4,12 +4,16 @@ class View {
   constructor (divId) {
     const self = this
     this._rootElement = divId
-    this._UploadandViewGallery = $('<div> <button class="btn btn-success" data-toggle="modal" data-target="#___fileupload">Upload File</button> </div>')
+    this._uploadButton = $('<div> <button class="btn btn-success">Upload File</button> </div>')
+    $(this._uploadButton).on('click', (e) => {
+      this._modalUpload.modal('show')
+    })
+
     this._imageInitialization = $('<div class="row"></div>')
 
-    this._modalContentImage = $('<div id="modalUpload" class="modal fade" role="dialog" style="overflow-y:auto;"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload Image</h4> </div> <div class="modal-body"></div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
+    this._modalContentImage = $('<div class="modal fade" role="dialog" style="overflow-y:auto;"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Upload Image</h4> </div> <div class="modal-body"></div> <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
     $('body').append(this._modalContentImage)
-    this._modalContentImage.find('div.modal-body').append(this._UploadandViewGallery)
+    this._modalContentImage.find('div.modal-body').append(this._uploadButton)
     this._modalContentImage.find('div.modal-body').append(this._imageInitialization)
     this._buttonNextCursor = $('<button class="btn btn-primary" data-next="">LOAD MORE</button>')
     this._linkInsideTextImage = $('<a href="javascript:void(0);" class="data-image-url" data-url="">Click to trigger Callback !!</a>')
@@ -19,12 +23,12 @@ class View {
     })
 
     // Modal section
-    this._modalDelete = $('<!-- Modal Delete File--><div id="___deletepicture" class="modal fade" role="dialog"> <div class="modal-dialog"> <!-- Modal content--> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Delete</h4> </div> <div class="modal-body"> <p>Are you sure you want to delete this picture?</p> </div> <div class="modal-footer"> <button type="button" class="btn btn-danger btn-delete">YES</button> <button type="button" class="btn btn-default" data-dismiss="modal">NO</button> </div> </div> </div></div>')
+    this._modalDelete = $('<!-- Modal Delete File--><div class="modal fade" role="dialog"> <div class="modal-dialog"> <!-- Modal content--> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Delete</h4> </div> <div class="modal-body"> <p>Are you sure you want to delete this picture?</p> </div> <div class="modal-footer"> <button type="button" class="btn btn-danger btn-delete">YES</button> <button type="button" class="btn btn-default" data-dismiss="modal">NO</button> </div> </div> </div></div>')
     this._modalDeleteCompleted = $('<div class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Delete Successful</h4> </div><div class="modal-body"> <p class="text-center">Your image has been deleted.</p></div><div class="modal-footer" style="text-align:center"> <button type="button" class="btn btn-default" data-dismiss="modal">Back to Image Gallery</button> </div></div></div></div>')
     this._modalDeleteFailed = $('<div class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Delete Failed</h4> </div><div class="modal-body"> <p class="text-center">Internal Server Error!</p></div><div class="modal-footer" style="text-align:center"> <button type="button" class="btn btn-default" data-dismiss="modal">Back to Image Gallery</button> </div></div></div></div>')
-    this._modalUpload = $('<!-- Modal Upload File--><div id="___fileupload" class="modal fade" role="dialog"> <div class="modal-dialog"> <!-- Modal content--> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File</h4> </div> <div class="modal-body"> <p>Below is the button to upload file : </p> <form><div class="form-group"><label for="uploadbutton">Upload File:</label><input id="fileupload" type="file" name="image[]" ></div> </form>  <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div> </div> </div></div>')
-    this._modalUploadSuccess = $('<div id="___fileuploadsuccess" class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File Success</h4> </div><div class="modal-body"> <div class="info_result text-center hidden"> <p>Image Uploaded Successfully!</p> <!-- <img src="" class="img-responsive result_image" style="margin:0 auto;"> <input type="text" readonly value="" class="form-control result_image_text"> --> </div></div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
-    this._modalUploadFailed = $('<div id="___fileuploadfailed" class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File Failed</h4> </div><div class="modal-body"> <div class="info_result text-center"> <p class="text_message_failed">Failed upload your file !</p></div></div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
+    this._modalUpload = $('<!-- Modal Upload File--><div class="modal fade" role="dialog"> <div class="modal-dialog"> <!-- Modal content--> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File</h4> </div> <div class="modal-body"> <p>Below is the button to upload file : </p> <form><div class="form-group"><label for="uploadbutton">Upload File:</label><input id="fileupload" type="file" name="image[]" ></div> </form>  <div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div> </div> </div></div>')
+    this._modalUploadSuccess = $('<div class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File Success</h4> </div><div class="modal-body"> <div class="info_result text-center hidden"> <p>Image Uploaded Successfully!</p> <!-- <img src="" class="img-responsive result_image" style="margin:0 auto;"> <input type="text" readonly value="" class="form-control result_image_text"> --> </div></div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
+    this._modalUploadFailed = $('<div class="modal fade" role="dialog"> <div class="modal-dialog"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Upload File Failed</h4> </div><div class="modal-body"> <div class="info_result text-center"> <p class="text_message_failed">Failed upload your file !</p></div></div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>')
 
     this._publicId = ''
     this._imgThumbnail = ''
@@ -97,7 +101,10 @@ class View {
     var textImage = $('<div class="text-center textImage" style=""></div>')
     var overlayImage = $('<div class="overlay" > </div>')
     var imageSrc = $('<img src="' + imageURL + '" class="img-responsive" style="height: 200px; max-height: 100%">')
-    var btnDeleteImage = $(`<div class="btn btn-danger btn-close" style="float:right" data-toggle="modal" data-target="#___deletepicture" data-public-id="${imagePublicId}"> <span>X</span> </div>`)
+    var btnDeleteImage = $(`<div class="btn btn-danger btn-close" style="float:right" data-public-id="${imagePublicId}"> <span>X</span> </div>`)
+    btnDeleteImage.on('click', () => {
+      this._modalDelete.modal('show')
+    })
     var imageHref = $(`<a href="javascript:void(0);" class="data-image-url" data-url="${imageURL}" data-public-id="${imagePublicId}">Select Image</a>`)
 
     textImage.append(imageHref)
